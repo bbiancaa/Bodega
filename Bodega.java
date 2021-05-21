a440f8dc8e4815d5cc272a8df566c5e50979a3e9
bbiancaa <biancagabriela.fritsch@hotmail.com>
2021-05-21 16:02:34 -0300

    inicio do sistema bodega, implementação do menu

---
+++ Bodega.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Bodega{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int x;
+      System.out.println("Digite a opção desejeada: \n");
+      System.out.println("1 - Cadastrar bebida ");
+      System.out.println("2 - Mostrar bebidas ");
+      System.out.println("3 - Comprar bebida ");
+      System.out.println("4 - Vender bebida ");
+      System.out.println("5 - Cadastrar cliente ");
+      System.out.println("6 - Mostrar clientes ");
+      System.out.println("7 - Sair do sistema ");
+      x = scan.nextInt();
+
+  }
+}
